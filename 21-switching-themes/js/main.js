c217add10a1cada094a78db97943e0e0d10710d6
Nuria <soriano.nuria@gmail.com>
2021-03-19 15:02:26 +0100

    remove unused variables

--- 21-switching-themes/js/main.js
+++ 21-switching-themes/js/main.js
@@ -1,10 +1,8 @@
 function setUpThemeToggle() {
   const themeBtn = document.getElementById("themeToggle");
-  let isDarkThemeOn = false;
 
   themeBtn.addEventListener("click", () => {
     document.documentElement.classList.toggle("dark-theme");
-    isDarkThemeOn = !isDarkThemeOn;
   });
 }
 
--- 33-images/js/main.js
+++ 33-images/js/main.js
@@ -1,10 +1,8 @@
 function setUpThemeToggle() {
   const themeToggle = document.getElementById("themeToggle");
-  let isDarkThemeOn = false;
 
   themeToggle.addEventListener("change", () => {
     document.documentElement.classList.toggle("dark-theme");
-    isDarkThemeOn = !isDarkThemeOn;
   });
 }
 
